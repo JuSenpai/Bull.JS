07a07906d053c75c7a936e947d2a80eb9f140cf0
Cosmin STOICA <cosmin.stoica@zitec.com>
2018-04-02 17:44:05 +0300

    implemented basic routing system

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const YAML = require('yamljs');
+
+const Bull = {
+    serializer: YAML,
+    init: function () {
+        try {
+            this.router.parseRoutes();
+        } catch(e) {
+            return;
+        }
+
+        app.listen(3000, "0.0.0.0", () => console.log("Server listening..."));
+    },
+
+    setRootFolder(root) {
+        this.root = root;
+        this.init();
+    },
+
+    router: {
+        routes: {},
+        defineRoute: function (route, controller, methods = ["GET"]) {
+            const controllerClass = controller.split(":")[0];
+            const controllerMethod = controller.split(":")[1];
+            this.routes[route] = methods;
+            methods.forEach(method => {
+                app[method.toLowerCase()](route, (req, res) => {
+                    controller = require(`/src/controller/${controllerClass}`);
+                    controller[`${controllerMethod}`](req, res, req.params);
+                });
+            });
+        },
+
+        parseRoutes: function () {
+            const routes = Bull.serializer.load(`${Bull.root}\\config\\routing.yml`).routes;
+            for(let route in routes) {
+                route = routes[route];
+                this.defineRoute(route.path, route.controller, route.methods);
+            }
+
+            return this.routes;
+        }
+    }
+};
+
+Bull.init();
+
+module.exports = Bull;
