aa10294825d42e0dd7d6b9f0de86bd78e7c13e00
Cosmin STOICA <cosmin.stoica@zitec.com>
2018-04-11 17:20:25 +0300

    having the same controller class for two different routes won't create two different instances of that class anymore

--- toolkit/routing.js
+++ toolkit/routing.js
@@ -6,13 +6,15 @@ const TemplateResponse = require("../Http/TemplateResponse");
 module.exports = function (app, config) {
     return {
         routes: {},
+        controllers: {},
         defineRoute: function (route, controller, methods = ["GET"]) {
             const controllerClass = controller.split(":")[0];
             const controllerMethod = controller.split(":")[1];
             this.routes[route] = methods;
             methods.forEach(method => {
                 app[method.toLowerCase()](route, (req, res) => {
-                    controller = require(`../../../src/controller/${controllerClass}`);
+                    controller = this.controllers[controllerClass] || require(`../../../src/controller/${controllerClass}`);
+                    this.controllers[controllerClass] = controller;
                     const response = controller[`${controllerMethod}Action`](req, req.params);
 
                     switch(true) {
@@ -23,7 +25,14 @@ module.exports = function (app, config) {
 
                         case response instanceof TemplateResponse:
                             res.setHeader("Content-Type", "text/html");
-                            res.render(response.path, Object.assign({}, response.params, config.templating.globals));
+                            if (config.engine) {
+                                res.render(response.path, Object.assign({}, response.params, config.templating.globals));
+                            } else {
+                                let string = `${controllerClass}::${controllerMethod}Action(): you tried to render a template, but no engine was specified in the config file.`;
+                                console.error(string);
+                                res.setHeader("Content-Type", "application/json");
+                                res.send(JSON.stringify({code: "error", message: string}));
+                            }
                             break;
 
                         case response instanceof Response:
