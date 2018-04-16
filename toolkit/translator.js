d4a0b889192bb6e1e37c79227a8154aa99365792
Cosmin STOICA <cosmin.stoica@zitec.com>
2018-04-16 17:10:06 +0300

    added basic translator

--- toolkit/translator.js
+++ toolkit/translator.js
@@ -3,20 +3,28 @@ const YAML = require("yamljs");
 
 module.exports = function(config) {
     return {
+        translations: {},
         load: function (locale) {
-            system.readDir(config.translations.root + "/" + locale)
-                .then(function* (contents) {
+            system.readDir(config.translations.root + "/" + locale + "/")
+                .then(function (contents) {
+                    let translations = {};
                     for (let file in contents) {
-                        yield YAML.parse(contents[file]);
+                        translations = Object.assign({}, translations, YAML.parse(contents[file].toString()).translations);
                     }
+                    return translations;
                 })
                 .then(translations => {
-                    console.log(translations);
+                    this.translations[locale] = translations;
                 });
         },
 
         translate: function (mask, locale = "en", params = {}) {
+            let translation = this.translations[locale][mask];
+            for(let param in params) {
+                translation = translation.replace(new RegExp(`:${param}`), params[param]);
+            }
 
+            return translation;
         }
     };
 };
